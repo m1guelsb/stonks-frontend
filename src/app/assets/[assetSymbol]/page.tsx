10eb49db4c6639059fe5f78e020fcda8c0757e1b
Miguel Barbosa <miguelbarbosacs@gmail.com>
2025-02-28 10:19:14 -0300

    feat: add asset details page

---
+++ src/app/assets/[assetSymbol]/page.tsx
@@ -0,0 +1,38 @@
+import { Card } from 'flowbite-react';
+import { AssetItem } from '@/components/AssetItem';
+import { AssetTabs } from '@/components/AssetTabs';
+import { Asset } from '@/models';
+import { AssetChatComponent } from './AssertChartComponent';
+
+export const getAsset = async (symbol: string): Promise<Asset> =>
+  fetch(`${process.env.API_URL}/assets/${symbol}`).then((res) => res.json());
+
+export default async function AssetDetails({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ assetSymbol: string }>;
+  searchParams: Promise<{ wallet_id: string }>;
+}) {
+  const walletId = (await searchParams).wallet_id;
+  const asset = await getAsset((await params).assetSymbol);
+
+  return (
+    <div className="flex flex-col gap-5 flex-grow">
+      <div className="flex flex-col gap-2">
+        <AssetItem asset={asset} />
+        <div className="font-bold text-2xl">${asset.price}</div>
+      </div>
+      <div className="grid grid-cols-5 flex-grow gap-24">
+        <div className="col-span-2">
+          <Card>
+            <AssetTabs asset={asset} walletId={walletId} />
+          </Card>
+        </div>
+        <div className="col-span-3 flex flex-grow">
+          <AssetChatComponent asset={asset} />
+        </div>
+      </div>
+    </div>
+  );
+}
